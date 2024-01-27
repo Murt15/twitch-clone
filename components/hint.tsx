225873221cd97b2bda8ace8c28238af1d37c5582
Murtaza Topiwala <112171962+Murt15@users.noreply.github.com>
2024-01-27 15:57:21 +0530

    add video component for dashboard

--- components/hint.tsx
+++ components/hint.tsx
@@ -4,7 +4,7 @@ interface HintProps {
     label: string;
     children: React.ReactNode;
     asChild?: boolean;
-    side: "top" | "bottom" | "left" | "right";
+    side?: "top" | "bottom" | "left" | "right";
     align?: "start" | "center" | "end";
 }
 
